4824cc931228f84fc27a299e9c5c50a144503646
pandiribhumika <bhumipersonal123@gmail.com>
2024-07-09 10:15:12 +0530

    Create MaxOfSubarray.java

---
+++ MaxOfSubarray.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Test {
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        int max=0;
+        for(int i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                int sum=0;
+                for(int k=i;k<=j;k++)
+                {
+                sum=sum+a[k];
+                }
+                if(max<sum)
+                max=sum;
+            }
+        }
+        System.out.println(max);
+    }
+}
